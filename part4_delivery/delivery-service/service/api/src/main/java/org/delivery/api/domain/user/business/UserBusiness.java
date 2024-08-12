cfb7c01d88e589ae1af649de2e5355733a3eeb3c
jeongho <leejh9128@gmail.com>
2024-08-12 13:22:32 +0900

    JWT 토큰 기반 로그인 기능 구현
    API 요청에 따른 JWT 토큰 기반 로그인 구현

--- part4_delivery/delivery-service/service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ part4_delivery/delivery-service/service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
 import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.business.TokenBusiness;
+import org.delivery.api.domain.token.controller.model.TokenResponse;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
@@ -20,6 +23,8 @@ public class UserBusiness {
     private final UserService userService;
 
     private final UserConverter userConverter;
+
+    private final TokenBusiness tokenBusiness;
 
     /**
      * 사용자에 대한 가입처리 로직
@@ -43,4 +48,24 @@ public class UserBusiness {
 //                .map(userConverter::toResponse)
 //                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "request null"));
     }
+
+    /**
+     * 1. email, password 를 가지고 사용자 체크
+     * 2. user entity 로그인 확인
+     * 3. token 생성
+     * 4. token response
+     */
+    public TokenResponse login(UserLoginRequest request) {
+        var userEntity = userService.login(request.getEmail(), request.getPassword());
+        var tokenResponse = tokenBusiness.issueToken(userEntity);
+        return tokenResponse;
+    }
+
+//    public UserResponse me(
+//            User user
+//    ) {
+//        var userEntity = userService.getUserWithThrow(user.getId());
+//        var response = userConverter.toResponse(userEntity);
+//        return response;
+//    }
 }
