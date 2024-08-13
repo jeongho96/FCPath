2749a1a1c0fca5de4a876de5a312bc0fba0f4a16
jeongho <leejh9128@gmail.com>
2024-08-13 13:18:28 +0900

    store 등록 및 검색 기능 개발
    간단한 기능 초안 작성 및 추가(register, search)

--- part4_delivery/delivery-service/service/api/src/main/java/org/delivery/api/interceptor/AuthorizationInterceptor.java
+++ part4_delivery/delivery-service/service/api/src/main/java/org/delivery/api/interceptor/AuthorizationInterceptor.java
@@ -26,19 +26,18 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("AuthorizationInterceptor url : {}", request.getRequestURI());
+        log.info("Authorization Interceptor url : {}", request.getRequestURI());
 
-        // Web, chrome의 경우 GET, POST OPTIONS = pass
-        if(HttpMethod.OPTIONS.matches(request.getMethod())) {
+        // WEB ,chrome 의 경우 GET, POST OPTIONS = pass
+        if(HttpMethod.OPTIONS.matches(request.getMethod())){
             return true;
         }
 
-        // js, html, png resource를 요청하는 경우 = pass
-        if(handler instanceof ResourceHttpRequestHandler) {
+        // js. html. png resource 를 요청하는 경우 = pass
+        if(handler instanceof ResourceHttpRequestHandler){
             return true;
         }
 
-        // TODO header 검증
         var accessToken = request.getHeader("authorization-token");
         if(accessToken == null){
             throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
@@ -54,6 +53,5 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
 
 
         throw new ApiException(ErrorCode.BAD_REQUEST, "인증실패");
-
     }
 }
