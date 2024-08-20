a7ccb117980bfb5fde93060b1666f9de1bb80578
jeongho <leejh9128@gmail.com>
2024-08-20 15:53:40 +0900

    SSE를 활용한 데이터 전송
    가맹점 파트너 user에게 주문 메시지 전송하기

--- part4_delivery/delivery-service/service/store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/model/UserOrderResponse.java
+++ part4_delivery/delivery-service/service/store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/model/UserOrderResponse.java
@@ -1,14 +1,10 @@
-package org.delivery.db.userorder;
+package org.delivery.storeadmin.domain.userorder.controller.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import org.delivery.db.BaseEntity;
 import org.delivery.db.userorder.enums.UserOrderStatus;
-
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,24 +12,17 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-@SuperBuilder
-@Entity
-@Table(name = "user_order")
-public class UserOrderEntity extends BaseEntity {
+@Builder
+public class UserOrderResponse {
 
-    @Column(nullable = false)
-    private Long userId;    // user table 1:n
+    private Long id;
 
-    @Column(nullable = false)
     private Long storeId;
 
+    private Long userId;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 50, nullable = false)
     private UserOrderStatus status;
 
-    @Column(precision = 11, scale = 4, nullable = false)
     private BigDecimal amount;
 
     private LocalDateTime orderedAt;
@@ -45,5 +34,4 @@ public class UserOrderEntity extends BaseEntity {
     private LocalDateTime deliveryStartedAt;
 
     private LocalDateTime receivedAt;
-
 }
